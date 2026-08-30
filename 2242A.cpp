3b4eb24bca47c03123dd92c0ae96bad011cbb472
Abdul Rafay <i240824@isb.nu.edu.pk>
2026-08-30 20:33:20 +0500

    removed infinite loop bug

--- 2242A.cpp
+++ 2242A.cpp
@@ -1,65 +1,66 @@
-#include<iostream>
-#include<stack>
-#include<vector>
+#include <iostream>
+#include <vector>
+
 using namespace std;
-bool checkCommon(string& a , stack<string>& ms){
-  while(!ms.empty()){
-    string b = ms.top();
-    ms.pop();
-    if (a == b){
-      return true;
+
+void solve() {
+    int k;
+    cin >> k;
+    vector<int> myvect(k);
+    for (int i = 0; i < k; i++) {
+        cin >> myvect[i];
     }
-  }
-  return false;
-}
-int main(){
-    int t = 0;
-    cin>>t;
-    while(t--){
-      int k = 0;
-      cin>>k;
-      vector<int> myvect;
-      for(int i = 0; i < k; i++){
-         int c1;
-         cin>>c1;
-         myvect.push_back(c1);
-    }
-    // my vect <2,1,1,2,2,1> (aababbaab)
-    // we will look for either 2 consecutive occurrences of 2,2
-    // case aa , bb
-    int count1 = 0;
-    int count2 = 0;
-    for(int i = 0; i < myvect.size(); i++){
-          if(myvect[i]==2 && i%2==0){
-            count1++;
-          }else if (myvect[i]==2 && i%2 != 0){
-            count2++;
-          }
-          if(count1 > 1 || count2 > 1){
-            cout<<"YES"<<endl;
-          }
-    } 
-    // case ab , ba
-    bool flag = false;
-    for(int i = 0; i < myvect.size()-1; i++){
-        int number1 = myvect[i];
-        number1+=(myvect[i+1]*10);
-        if(number1>10){
-      for(int k = i + 2; k < myvect.size() - 2; i+=2){
-         int number2 = myvect[k];
-         number2 = (myvect[k + 1])*10;
-         if(number2>10){
-            cout<<"YES"<<endl;
-            flag = true;
+
+    int count1 = 0, count2 = 0;
+    bool found = false;
+
+    // Case 1: Checking parity positions
+    for (int i = 0; i < k; i++) {
+        if (myvect[i] == 2) {
+            if (i % 2 == 0) count1++;
+            else count2++;
+        }
+        if (count1 > 1 || count2 > 1) {
+            found = true;
             break;
-         }
-      }
-      if(flag){
-        break;
-      }
+        }
     }
-  }
-  if(!flag){
-  cout<<"No"<<endl;
-  }
+
+    if (found) {
+        cout << "YES\n";
+        return;
+    }
+
+    // Case 2: Pairwise checking without infinite loop
+    for (int i = 0; i < k - 1; i++) {
+        int number1 = myvect[i] + (myvect[i + 1] * 10);
+        if (number1 > 10) {
+            for (int j = i + 2; j < k - 1; j += 2) { // Fixed increment variable to j (or k)
+                int number2 = myvect[j] + (myvect[j + 1] * 10);
+                if (number2 > 10) {
+                    found = true;
+                    break;
+                }
+            }
+        }
+        if (found) break;
+    }
+
+    if (found) {
+        cout << "YES\n";
+    } else {
+        cout << "NO\n";
+    }
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int t;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
 }
